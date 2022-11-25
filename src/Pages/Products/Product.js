8dac6394197ca1c010016be5a7b2b02a1ccfc5b2
Abdullah Al Mubin <amttawsik99@gmail.com>
2022-11-25 13:22:31 +0600

    Booking work done!

--- src/Pages/Products/Product.js
+++ src/Pages/Products/Product.js
@@ -1,34 +1,217 @@
-import React from 'react';
-import blueTick from '../../assects/blueTick.png'
+import React, { useContext } from "react";
+import blueTick from "../../assects/blueTick.png";
+import { AuthContext } from "../../contexts/AuthContext";
 
-const Product = ({product}) => {
-    return (
-        <div>
-            <div key={product._id} className="card w-100 bg-base-100 shadow-xl">
-            <figure>
-              <img className="w-full" src={product.img} alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                {product.productName}
-                <div className="badge badge-secondary">NEW</div>
-              </h2>
-              <div>
-              <p>Location: {product.sellerLocation}</p>
-              <p>Resale Price: ${product.resalePrice}</p>
-              <p>Orginal Price: ${product.orgPrice}</p>
-              <p>Years Of Use: {product.yearsOfUse} Years</p>
-              <p>Post Time: {product.postDate}</p>
-              <p className="flex items-center">Saler Name: {product.isVarified && <span className="mt-1"><img style={{width:'15px'}} src={blueTick} alt="" /></span>}{product.sellerName}</p>
-              </div>
-              <div className="card-actions justify-end">
-                <div className="badge badge-outline">Report to Admin</div>
-                <div className="badge badge-outline">Book Now</div>
-              </div>
-            </div>
+const Product = ({ product }) => {
+  const { user } = useContext(AuthContext);
+  const {
+    img,
+    _id,
+    productName,
+    isVarified,
+    sellerLocation,
+    resalePrice,
+    orgPrice,
+    yearsOfUse,
+    postDate,
+    sellerName,
+  } = product;
+  const handleConfirmOrder = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const buyersName = form.name.value;
+    const buyersEmail = form.email.value;
+    const productName = form.productName.value;
+    const resalePrice = form.price.value;
+    const number = form.number.value;
+    const location = form.location.value;
+    const booking = {
+      buyersName,
+      buyersEmail,
+      productName,
+      resalePrice,
+      number,
+      location,
+      img,
+    }
+    console.log(booking)
+    fetch("http://localhost:5000/booking", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  
+  };
+  return (
+    <div>
+      <div className="card w-100 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            style={{ height: "320px" }}
+            className="w-full"
+            src={img}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {productName}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <div>
+            <p>Location: {sellerLocation}</p>
+            <p>Resale Price: ${resalePrice}</p>
+            <p>Orginal Price: ${orgPrice}</p>
+            <p>Years Of Use: {yearsOfUse} Years</p>
+            <p>Post Time: {postDate}</p>
+            <p className="flex items-center">
+              Saler Name:{" "}
+              {isVarified && (
+                <span className="mt-1">
+                  <img style={{ width: "15px" }} src={blueTick} alt="" />
+                </span>
+              )}
+              {sellerName}
+            </p>
+          </div>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Report to Admin</div>
+            <label htmlFor="my-modal" className="badge badge-outline">
+              Book Now
+            </label>
           </div>
         </div>
-    );
+      </div>
+
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Book This Product Now!</h3>
+          <form onSubmit={handleConfirmOrder}>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-primary undefined"
+              >
+                Full Name
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  readOnly
+                  defaultValue={user.displayName}
+                  type="text"
+                  name="name"
+                  className="input input-bordered input-primary w-full"
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-primary undefined"
+              >
+                Email
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  readOnly
+                  defaultValue={user.email}
+                  type="text"
+                  name="email"
+                  className="input input-bordered input-primary w-full "
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="productName"
+                className="block text-sm font-medium text-primary undefined"
+              >
+                Product Name
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  readOnly
+                  defaultValue={productName}
+                  type="text"
+                  name="productName"
+                  className="input input-bordered input-primary w-full "
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="Price"
+                className="block text-sm font-medium text-primary undefined"
+              >
+                Price
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  readOnly
+                  defaultValue={`$${resalePrice}`}
+                  type="Price"
+                  name="price"
+                  className="input input-bordered input-primary w-full "
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="number"
+                className="block text-sm font-medium text-primary undefined"
+              >
+                Phone Number
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="text"
+                  name="number"
+                  className="input input-bordered input-primary w-full "
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="location"
+                className="block text-sm font-medium text-primary undefined"
+              >
+                Meating Location
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="text"
+                  name="location"
+                  className="input input-bordered input-primary w-full "
+                  required
+                />
+              </div>
+            </div>
+            <button type="submit" className="modal-action">
+              <label htmlFor="my-modal" className="btn">
+                Confirm Submit
+              </label>
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
