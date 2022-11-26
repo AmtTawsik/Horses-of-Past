c3f602e4065ff95a17d2d8bbab530dea63d8df55
Abdullah Al Mubin <amttawsik99@gmail.com>
2022-11-27 00:26:30 +0600

    checkout from complited

---
+++ src/Pages/Dashboard/Payment/CheckoutForm.js
@@ -0,0 +1,65 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
+
+const CheckoutForm = () => {
+  const [cardError, setCardError] = useState("");
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log(error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-info btn-sm my-5 "
+          type="submit"
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default CheckoutForm;
--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,19 +1,35 @@
-import { loadStripe } from '@stripe/stripe-js';
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import CheckoutForm from "./CheckoutForm";
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK)
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
-    const bookingData = useLoaderData();
-    console.log(bookingData)
-    return (
-        <div>
-            <h3 className='text-center text-secondary font-bold text-4xl'>Payment for {bookingData.productName}</h3>
-            <p className='text-3xl text-center'>Please Pay <strong className='text-green-500'>${bookingData.resalePrice}</strong> to buy this <span className='text-green-500 font-bold'>{bookingData.productName}</span></p>
-            <div className="divider">Pay Now!!!</div>
-        </div>
-    );
+  const bookingData = useLoaderData();
+  console.log(bookingData);
+  return (
+    <div>
+      <h3 className="text-center text-secondary font-bold text-4xl">
+        Payment for {bookingData.productName}
+      </h3>
+      <p className="text-3xl text-center">
+        Please Pay{" "}
+        <strong className="text-green-500">${bookingData.resalePrice}</strong>{" "}
+        to buy this{" "}
+        <span className="text-green-500 font-bold">
+          {bookingData.productName}
+        </span>
+      </p>
+      <div className="divider">Pay Now!!!</div>
+      <div className="w-96 my-12">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
